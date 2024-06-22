bccf81d28c751820e714180877ab1ae890004b19
Ayush Kumar <ayushk2020@gmail.com>
2024-06-21 22:01:02 -0400

    bgfix 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styles from "./page.module.scss";
+import img from "../public/lore.jpg";
 
 export default function Home() {
 
   const imageUrl = '/lore.jpg'
   return (
     <main className={styles.main}>
-      <div className={styles.container} style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className={styles.container} style={{ backgroundImage: `url(${img.src})` }}>
         <header>
           <h1 className={styles.title}>{"<ancouver.de>"}</h1>
         </header>
